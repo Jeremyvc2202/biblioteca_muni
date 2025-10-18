c66a31f5c8a01bfc5bf460555c5017c920f629b8
Jeremyvc2202 <Jeremyvc2202@users.noreply.github.com>
2025-10-18 15:03:37 -0500

    retoques finales, icnorporacion de un icono

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <title>Agenda Juanita</title>
   <link rel="stylesheet" href="assets/styles.css">
+  <link rel="icon" type="image/x-icon" href="assets/libros/biblioteca.ico">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
